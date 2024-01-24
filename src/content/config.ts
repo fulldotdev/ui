c81de43a3f71d46b1644a895b50836bb4fe93291
Dylan Kuipers <dylankuipers10@gmail.com>
2024-01-24 10:27:18 +0100

    content

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,24 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { defineCollection, z } from 'astro:content'
+
+const content = defineCollection({
+  type: 'content',
+  schema: z.any(),
+})
+
+const data = defineCollection({
+  type: 'data',
+  schema: z.any(),
+})
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
-};
+  categories: content,
+  pages: content,
+  policies: content,
+  posts: content,
+  products: content,
+  reviews: content,
+  services: content,
+  globals: data,
+  settings: data,
+  blocks: data,
+}
