aa530554a8d0bda068b13880a55a097bacc1f0c1
Sil <silveltman@gmail.com>
2024-01-30 20:04:14 +0100

    testing with indexts exports

---
+++ src/components/elements/index.js
@@ -0,0 +1,12 @@
+import CardRoot from './card/CardRoot.astro'
+import CardSegment from './card/CardSegment.astro'
+import CardWriteup from './card/CardWriteup.astro'
+
+export {
+  CardRoot,
+  CardSegment,
+  CardWriteup,
+  CardRoot as Root,
+  CardSegment as Segment,
+  CardWriteup as Writeup,
+}
