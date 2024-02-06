6d34863e4ba36861766dee8b08eb0d0b0205df33
Sil <silveltman@gmail.com>
2024-02-06 09:14:48 +0100

    Add category reference prop to CategoriesCard and update Card component
    Add Section component and its related files

---
+++ src/components/elements/section/index.ts
@@ -0,0 +1,14 @@
+import Section from './Section.astro'
+import Root from './SectionRoot.astro'
+import Segment from './SectionSegment.astro'
+import Writeup from './SectionWriteup.astro'
+
+export {
+  Root,
+  Section,
+  Root as SectionRoot,
+  Segment as SectionSegment,
+  Writeup as SectionWriteup,
+  Segment,
+  Writeup,
+}
