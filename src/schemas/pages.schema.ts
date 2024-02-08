4b11d0753f899d3743e90bbf6a17760830ba418c
Sil <silveltman@gmail.com>
2024-02-08 12:08:36 +0100

    @dylan

--- src/schemas/pages.schema.ts
+++ src/schemas/pages.schema.ts
@@ -4,5 +4,7 @@ export const pagesSchema = defineCollection({
   type: 'data',
   // @dylan deze any vervangen voor een zod schema. Je kunt de .md schema gebruiken als voorbeeld.
   // Chatgpt kan je helpen dit om te zetten naar een zod schema. Zie ook de zod docs
+  // Zie ook de astro content collection docs
+  // de 'sections' key is een array met sections. Deze is alleen wel heel complex, dus deze mag je op any zetten
   schema: z.any(),
 })
