0cc336c0b0e95dbf8d788a4642e7e2e7765d622d
Sil <silveltman@gmail.com>
2024-03-05 15:31:10 +0100

    ready for merge with main

--- src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Layout } from './Layout.astro'
-export { default as LayoutBody } from './LayoutBody.astro'
-export { default as LayoutMain } from './LayoutMain.astro'
-export { default as LayoutRoot } from './LayoutRoot.astro'
