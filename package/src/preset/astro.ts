a7cb0a95a6d61af6a88a7371ee3112006f20b923
Sil <silveltman@gmail.com>
2024-03-19 13:04:26 +0100

    Refactor Astro integration in package/src/preset/astro.ts

--- package/src/preset/astro.ts
+++ package/src/preset/astro.ts
@@ -1,24 +1,55 @@
-import { default as UnoCSS } from '@unocss/astro'
+import UnoCSS from '@unocss/astro'
 import type { AstroIntegration } from 'astro'
 import type { UnoConfig } from './uno'
 import preset from './uno'
 
 interface Config extends UnoConfig {}
 
+// export default function fullui(config?: Partial<Config>): AstroIntegration {
+//   return UnoCSS({
+//     injectReset: true,
+//     content: {
+//       pipeline: {
+//         include: [
+//           // the default
+//           /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+//           // include js/ts files
+//           'src/**/*.{js,ts,md}',
+//         ],
+//       },
+//       filesystem: ['src/**/*.md'],
+//     },
+//     presets: [preset(config)],
+//   })
+// }
+
 export default function fullui(config?: Partial<Config>): AstroIntegration {
-  return UnoCSS({
-    injectReset: true,
-    content: {
-      pipeline: {
-        include: [
-          // the default
-          /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
-          // include js/ts files
-          'src/**/*.{js,ts,md}',
-        ],
+  return {
+    name: 'banana-css-integration',
+    hooks: {
+      'astro:config:setup': ({ updateConfig }) => {
+        updateConfig({
+          vite: {
+            plugins: [
+              UnoCSS({
+                injectReset: true,
+                content: {
+                  pipeline: {
+                    include: [
+                      // the default
+                      /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+                      // include js/ts files
+                      'src/**/*.{js,ts,md}',
+                    ],
+                  },
+                  filesystem: ['src/**/*.md'],
+                },
+                presets: [preset(config)],
+              }),
+            ],
+          },
+        })
       },
-      filesystem: ['src/**/*.md'],
     },
-    presets: [preset(config)],
-  })
+  }
 }
