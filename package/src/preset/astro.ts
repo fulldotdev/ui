87fd586a1ca00e6913bd4481f7517e325905791d
Sil <silveltman@gmail.com>
2024-03-19 13:13:29 +0100

    Update Astro config and fullui integration

--- package/src/preset/astro.ts
+++ package/src/preset/astro.ts
@@ -5,51 +5,51 @@ import preset from './uno'
 
 interface Config extends UnoConfig {}
 
-// export default function fullui(config?: Partial<Config>): AstroIntegration {
-//   return UnoCSS({
-//     injectReset: true,
-//     content: {
-//       pipeline: {
-//         include: [
-//           // the default
-//           /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
-//           // include js/ts files
-//           'src/**/*.{js,ts,md}',
-//         ],
-//       },
-//       filesystem: ['src/**/*.md'],
-//     },
-//     presets: [preset(config)],
-//   })
-// }
-
 export default function fullui(config?: Partial<Config>): AstroIntegration {
-  return {
-    name: 'banana-css-integration',
-    hooks: {
-      'astro:config:setup': ({ updateConfig }) => {
-        updateConfig({
-          vite: {
-            plugins: [
-              UnoCSS({
-                injectReset: true,
-                content: {
-                  pipeline: {
-                    include: [
-                      // the default
-                      /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
-                      // include js/ts files
-                      'src/**/*.{js,ts,md}',
-                    ],
-                  },
-                  filesystem: ['src/**/*.md'],
-                },
-                presets: [preset(config)],
-              }),
-            ],
-          },
-        })
+  return UnoCSS({
+    injectReset: true,
+    content: {
+      pipeline: {
+        include: [
+          // the default
+          /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+          // include js/ts files
+          'src/**/*.{js,ts,md}',
+        ],
       },
+      filesystem: ['src/**/*.md'],
     },
-  }
+    presets: [preset(config)],
+  })
 }
+
+// export default function fullui(config?: Partial<Config>): AstroIntegration {
+//   return {
+//     name: 'banana-css-integration',
+//     hooks: {
+//       'astro:config:setup': ({ updateConfig }) => {
+//         updateConfig({
+//           vite: {
+//             plugins: [
+//               UnoCSS({
+//                 injectReset: true,
+//                 content: {
+//                   pipeline: {
+//                     include: [
+//                       // the default
+//                       /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+//                       // include js/ts files
+//                       'src/**/*.{js,ts,md}',
+//                     ],
+//                   },
+//                   filesystem: ['src/**/*.md'],
+//                 },
+//                 presets: [preset(config)],
+//               }),
+//             ],
+//           },
+//         })
+//       },
+//     },
+//   }
+// }
