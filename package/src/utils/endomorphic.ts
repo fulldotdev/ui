9532a02d2e40dbdadc9db63af01aaa8a6ffd9584
Sil <silveltman@gmail.com>
2024-03-21 13:53:45 +0100

    endomorphic working with zod

--- package/src/utils/endomorphic.ts
+++ package/src/utils/endomorphic.ts
@@ -1,46 +1,39 @@
 import type { AstroGlobal } from 'astro'
-import merge from 'deepmerge'
+import { z } from 'astro:content'
+import { merge } from 'merge-anything'
 
-export type Endomorphic<
-  Props extends AstroGlobal['props'],
-  Name extends string,
-  Value extends AstroGlobal['props'][Name] = undefined | null,
-> = {
-  [Key in Name | `_${Name}`]?:
-    | Value
-    | (Omit<Props, Name> & {
-        [K in Name]: Value | undefined
-      })
-    | null
-    | undefined
-}
-
+// Recursively add entire schema under name and _name
 export const endomorphic = <
-  Props extends Endomorphic<
-    AstroGlobal['props'],
-    Name,
-    AstroGlobal['props'][Name]
-  >,
-  Name extends string,
+  N extends keyof z.infer<S>,
+  S extends z.ZodObject<any, any, any>,
 >(
-  props: Props,
-  name: Name
-) => {
-  // @ts-ignore
-  const { [name]: value, [`_${name}`]: _value, ...stripped } = props
+  name: N & string,
+  schema: S
+) =>
+  schema.omit({ [name]: true }).extend({
+    [name]: schema.shape[name].or(schema).nullish(),
+    [`_${name}`]: schema.shape[name].or(schema).nullish(),
+  }) as z.ZodType<
+    Omit<z.infer<S>, N> & {
+      [K in N | `_${N & string}`]?: z.infer<S> | null | undefined
+    }
+  >
 
-  // @ts-ignore
-  const toObject = (val) =>
-    typeof val === 'object' && val !== null && !Array.isArray(val)
-      ? val
-      : { [name]: val }
-
-  const object = toObject(value)
-  const _object = toObject(_value)
-
-  // @ts-ignore
-  const merged = merge(stripped, merge(_object, object))
-
-  return merged as Omit<Props, Name | `_${Name}`> &
-    Extract<Props[Name], object & { length?: never }>
-}
+// Merges flat, name and _name properties
+export const ectomorphic = <
+  N extends keyof P,
+  S extends ReturnType<typeof endomorphic<N, z.ZodObject<any, any, any>>>,
+  P extends AstroGlobal['props'],
+>(
+  name: N & string,
+  schema: S,
+  props: P
+) =>
+  schema
+    .transform((val) => {
+      const stripped = { ...val }
+      delete val[name]
+      delete val[`_${name}`]
+      return merge(val[`_${name}`], val[name], stripped)
+    })
+    .parse(props) as P[`_${N & string}`] & P[N] & Omit<P, N | `_${N & string}`>
