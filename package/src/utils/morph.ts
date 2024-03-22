7059363532d9fe3cdfd523a31d5f639d44e1abe8
Sil <silveltman@gmail.com>
2024-03-22 10:59:04 +0100

    Refactor morph function in Writeup.astro and morph.ts

--- package/src/utils/morph.ts
+++ package/src/utils/morph.ts
@@ -1,5 +1,5 @@
 import type { AstroGlobal } from 'astro'
-import type { ZodRawShape } from 'astro/zod'
+import type { ZodObject, ZodRawShape } from 'astro/zod'
 import { z } from 'astro:content'
 import { merge } from 'merge-anything'
 import { schema as tag } from '../components/Tag.astro'
@@ -21,54 +21,21 @@ const mergeProps = (name: string, val: any) => {
   )
 }
 
-export const morph = <
-  N extends (keyof S & string) | string,
-  S extends ZodRawShape,
-  P extends AstroGlobal['props'],
->(
-  name: N,
-  shape: S
-) => {
-  const microSchema = z.object(shape)
-  const microParse = (props: P) => microSchema.parse(mergeProps(name, props))
-
-  const mesoSchema = tag.merge(microSchema)
-  const mesoParse = (props: P) => mesoSchema.parse(mergeProps(name, props))
-
-  const value = shape[name].or(mesoSchema).nullable()
-  const nested = {
-    [name]: value,
-    [`_${name}`]: value,
-  } as {
-    [K in N | `_${N}`]: typeof value
-  }
-
-  const macroSchema = mesoSchema.extend(nested)
-  const macroParse = (props: P | null) => macroSchema.parse(props)
-
-  return { micro: microParse, meso: mesoParse, macro: macroSchema }
-}
-
 // export const morph = <
 //   N extends (keyof S & string) | string,
-//   S extends ZodRawShape | ZodObject<any>,
+//   S extends ZodRawShape,
 //   P extends AstroGlobal['props'],
 // >(
 //   name: N,
-//   zod: S
+//   shape: S
 // ) => {
-//   const schema =
-//     typeof zod === 'function'
-//       ? (zod as ZodObject<any>)
-//       : z.object(zod as ZodRawShape)
-
-//   const microSchema = schema
+//   const microSchema = z.object(shape)
 //   const microParse = (props: P) => microSchema.parse(mergeProps(name, props))
 
 //   const mesoSchema = tag.merge(microSchema)
 //   const mesoParse = (props: P) => mesoSchema.parse(mergeProps(name, props))
 
-//   const value = schema.shape[name].or(mesoSchema).nullable()
+//   const value = shape[name].or(mesoSchema).nullable()
 //   const nested = {
 //     [name]: value,
 //     [`_${name}`]: value,
@@ -81,3 +48,34 @@ export const morph = <
 
 //   return { micro: microParse, meso: mesoParse, macro: macroSchema }
 // }
+
+export const morph = <
+  N extends (keyof S & string) | string,
+  S extends ZodRawShape | ZodObject<any>,
+  P extends AstroGlobal['props'],
+>(
+  name: N,
+  zod: S
+) => {
+  const schema =
+    'parse' in zod ? (zod as ZodObject<any>) : z.object(zod as ZodRawShape)
+
+  const microSchema = schema
+  const microParse = (props: P) => microSchema.parse(mergeProps(name, props))
+
+  const mesoSchema = tag.merge(microSchema)
+  const mesoParse = (props: P) => mesoSchema.parse(mergeProps(name, props))
+
+  const value = schema.shape[name].or(mesoSchema).nullable()
+  const nested = {
+    [name]: value,
+    [`_${name}`]: value,
+  } as {
+    [K in N | `_${N}`]: typeof value
+  }
+
+  const macroSchema = mesoSchema.extend(nested)
+  const macroParse = (props: P | null) => macroSchema.parse(props)
+
+  return { micro: microParse, meso: mesoParse, macro: macroSchema }
+}
