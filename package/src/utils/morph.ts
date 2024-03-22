28cfc05ae2fcf6be6914e52ca68593fc2601af51
Sil <silveltman@gmail.com>
2024-03-22 13:32:18 +0100

    Refactor components and update dependencies

--- package/src/utils/morph.ts
+++ package/src/utils/morph.ts
@@ -1,5 +1,5 @@
 import type { AstroGlobal } from 'astro'
-import type { ZodObject, ZodRawShape } from 'astro/zod'
+import type { ZodRawShape } from 'astro/zod'
 import { z } from 'astro:content'
 import { merge } from 'merge-anything'
 import { schema as tag } from '../components/Tag.astro'
@@ -21,52 +21,22 @@ const mergeProps = (name: string, val: any) => {
   )
 }
 
-// export const morph = <
-//   N extends (keyof S & string) | string,
-//   S extends ZodRawShape,
-//   P extends AstroGlobal['props'],
-// >(
-//   name: N,
-//   shape: S
-// ) => {
-//   const microSchema = z.object(shape)
-//   const microParse = (props: P) => microSchema.parse(mergeProps(name, props))
-
-//   const mesoSchema = tag.merge(microSchema)
-//   const mesoParse = (props: P) => mesoSchema.parse(mergeProps(name, props))
-
-//   const value = shape[name].or(mesoSchema).nullable()
-//   const nested = {
-//     [name]: value,
-//     [`_${name}`]: value,
-//   } as {
-//     [K in N | `_${N}`]: typeof value
-//   }
-
-//   const macroSchema = mesoSchema.extend(nested)
-//   const macroParse = (props: P | null) => macroSchema.parse(props)
-
-//   return { micro: microParse, meso: mesoParse, macro: macroSchema }
-// }
-
 export const morph = <
   N extends (keyof S & string) | string,
-  S extends ZodRawShape | ZodObject<any>,
+  S extends ZodRawShape,
   P extends AstroGlobal['props'],
 >(
   name: N,
-  zod: S
+  shape: S
 ) => {
-  const schema =
-    'parse' in zod ? (zod as ZodObject<any>) : z.object(zod as ZodRawShape)
-
-  const microSchema = schema
-  const microParse = (props: P) => microSchema.parse(mergeProps(name, props))
+  const microSchema = z.object(shape)
+  const microParse = (props: P) =>
+    microSchema.parse(mergeProps(name, props)) as z.infer<typeof microSchema>
 
   const mesoSchema = tag.merge(microSchema)
   const mesoParse = (props: P) => mesoSchema.parse(mergeProps(name, props))
 
-  const value = schema.shape[name].or(mesoSchema).nullable()
+  const value = microSchema.shape[name].or(mesoSchema).nullable()
   const nested = {
     [name]: value,
     [`_${name}`]: value,
@@ -79,3 +49,18 @@ export const morph = <
 
   return { micro: microParse, meso: mesoParse, macro: macroSchema }
 }
+
+// export const pick = <
+//   O extends ZodObject<ZodRawShape>,
+//   N extends keyof z.infer<O>,
+// >(
+//   object: O,
+//   name: N
+// ) => {
+//   return object.pick({
+//     [name as N & string]: true,
+//     [`_${name as N & string}`]: true,
+//   }) as ZodType<{
+//     [K in N]: z.infer<O>[N]
+//   }>
+// }
