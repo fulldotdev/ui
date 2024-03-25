5766c7733a4e4cadffa02be77beef94a5e50d870
Sil <silveltman@gmail.com>
2024-03-25 16:46:51 +0100

    fully remoeed astro:content usage (except for in astro components)

--- package/src/utils/getEntriesByProps.ts
+++ package/src/utils/getEntriesByProps.ts
@@ -1,33 +1,33 @@
-import {
-  getCollection,
-  type CollectionEntry,
-  type ContentCollectionKey,
-} from 'astro:content'
+// import {
+//   getCollection,
+//   type CollectionEntry,
+//   type ContentCollectionKey,
+// } from 'astro:content'
 
-export type EntriesReference =
-  | {
-      [Key in 'collection' | 'cards']?: ContentCollectionKey
-    }
-  | {
-      [Key in ContentCollectionKey]?: CollectionEntry<ContentCollectionKey>['slug'][]
-    }
+// export type EntriesReference =
+//   | {
+//       [Key in 'collection' | 'cards']?: ContentCollectionKey
+//     }
+//   | {
+//       [Key in ContentCollectionKey]?: CollectionEntry<ContentCollectionKey>['slug'][]
+//     }
 
-export const getEntriesByProps = async (
-  props: Partial<EntriesReference>
-): Promise<CollectionEntry<ContentCollectionKey>[] | undefined> => {
-  const collection = 'collection' in props ? props.collection : undefined
-  const cards = 'cards' in props ? props.cards : undefined
-  if (collection) return getCollection(collection)
-  if (cards) return getCollection(cards)
+// export const getEntriesByProps = async (
+//   props: Partial<EntriesReference>
+// ): Promise<CollectionEntry<ContentCollectionKey>[] | undefined> => {
+//   const collection = 'collection' in props ? props.collection : undefined
+//   const cards = 'cards' in props ? props.cards : undefined
+//   if (collection) return getCollection(collection)
+//   if (cards) return getCollection(cards)
 
-  const temp = console.warn // HACK
-  console.warn = () => {} // HACK
-  const promises = Object.keys(props).map((key) => {
-    // @ts-ignore
-    return getCollection(key, (entry) => props[key].includes(entry.slug))
-  })
-  const entries = (await Promise.all(promises)).flat()
-  console.warn = temp // HACK
+//   const temp = console.warn // HACK
+//   console.warn = () => {} // HACK
+//   const promises = Object.keys(props).map((key) => {
+//     // @ts-ignore
+//     return getCollection(key, (entry) => props[key].includes(entry.slug))
+//   })
+//   const entries = (await Promise.all(promises)).flat()
+//   console.warn = temp // HACK
 
-  return entries as CollectionEntry<ContentCollectionKey>[] | undefined // FIXME
-}
+//   return entries as CollectionEntry<ContentCollectionKey>[] | undefined // FIXME
+// }
--- package/src/utils/readEntryData.ts
+++ package/src/utils/readEntryData.ts
@@ -1,24 +1,24 @@
-import { type CollectionEntry, type CollectionKey } from 'astro:content'
-import fs from 'fs/promises' // Use fs promises for async operations
-import yaml from 'js-yaml'
-import path from 'path'
+// import { type CollectionEntry, type CollectionKey } from 'astro:content'
+// import fs from 'fs/promises' // Use fs promises for async operations
+// import yaml from 'js-yaml'
+// import path from 'path'
 
-type EntryData = Partial<CollectionEntry<CollectionKey>['data']>
+// type EntryData = Partial<CollectionEntry<CollectionKey>['data']>
 
-export const readEntryData = async (
-  collection: CollectionKey,
-  id: string
-): Promise<EntryData> => {
-  try {
-    const filepath = path.join(process.cwd(), `src/content/${collection}/${id}`)
-    let file = await fs.readFile(`${filepath}`, 'utf8')
+// export const readEntryData = async (
+//   collection: CollectionKey,
+//   id: string
+// ): Promise<EntryData> => {
+//   try {
+//     const filepath = path.join(process.cwd(), `src/content/${collection}/${id}`)
+//     let file = await fs.readFile(`${filepath}`, 'utf8')
 
-    const isMarkdown = id.endsWith('.md')
-    const regex = /(?<=---)[\s\S]*?(?=---)/
-    const yamlString = isMarkdown ? file.match(regex)?.[0] : file
+//     const isMarkdown = id.endsWith('.md')
+//     const regex = /(?<=---)[\s\S]*?(?=---)/
+//     const yamlString = isMarkdown ? file.match(regex)?.[0] : file
 
-    return yaml.load(yamlString || '') || ({} as EntryData)
-  } catch (error) {
-    return {}
-  }
-}
+//     return yaml.load(yamlString || '') || ({} as EntryData)
+//   } catch (error) {
+//     return {}
+//   }
+// }
