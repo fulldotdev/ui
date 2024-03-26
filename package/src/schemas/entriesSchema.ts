b5402adeb833a88860f80907b606bd415e99be75
Sil <silveltman@gmail.com>
2024-03-26 18:05:51 +0100

    Add entrySchema to Card component and remove unnecessary code from entriesSchema

--- package/src/schemas/entriesSchema.ts
+++ package/src/schemas/entriesSchema.ts
@@ -1,12 +1,9 @@
 import type { CollectionEntry, ContentCollectionKey } from 'astro:content'
-import { ZodType, z } from 'zod'
+import { z } from 'zod'
 import { entrySchema } from './entrySchema'
 
 export type EntrySlug = CollectionEntry<ContentCollectionKey>['slug']
 export type Entries = z.infer<typeof entriesSchema>
-
-const collection = z.string() as ZodType<ContentCollectionKey>
-const slug = z.string() as ZodType<EntrySlug>
 
 export const entriesSchema = z
   .array(entrySchema)
