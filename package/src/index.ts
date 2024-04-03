26ed28d939d1f6087cba533db979bcc4c0f27e82
lenardsilvester <lenardsilvester@icloud.com>
2024-04-03 11:21:39 +0200

    badge component

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,80 +1,82 @@
 // Utils
-export * from './utils/getPathname';
+export * from './utils/getPathname'
 // schemas
-export * from './schemas/attributesGroupedSchema';
-export * from './schemas/attributesSchema';
-export * from './schemas/collectionEntrySchema';
-export * from './schemas/collectionSchema';
-export * from './schemas/elementsSchema';
-export * from './schemas/entriesSchema';
-export * from './schemas/entrySchema';
-export * from './schemas/iconsSchema';
+export * from './schemas/attributesGroupedSchema'
+export * from './schemas/attributesSchema'
+export * from './schemas/collectionEntrySchema'
+export * from './schemas/collectionSchema'
+export * from './schemas/elementsSchema'
+export * from './schemas/entriesSchema'
+export * from './schemas/entrySchema'
+export * from './schemas/iconsSchema'
 // Components
-export * from './components/Action.astro';
-export { default as Action } from './components/Action.astro';
-export * from './components/Button.astro';
-export { default as Button } from './components/Button.astro';
-export * from './components/Card.astro';
-export { default as Card } from './components/Card.astro';
-export * from './components/Checkbox.astro';
-export { default as Checkbox } from './components/Checkbox.astro';
-export * from './components/Composite.astro';
-export { default as Composite } from './components/Composite.astro';
-export * from './components/Deck.astro';
-export { default as Deck } from './components/Deck.astro';
-export * from './components/Description.astro';
-export { default as Description } from './components/Description.astro';
-export * from './components/Display.astro';
-export { default as Display } from './components/Display.astro';
-export * from './components/Field.astro';
-export { default as Field } from './components/Field.astro';
-export * from './components/Form.astro';
-export { default as Form } from './components/Form.astro';
-export * from './components/Head.astro';
-export { default as Head } from './components/Head.astro';
-export * from './components/Header.astro';
-export { default as Header } from './components/Header.astro';
-export * from './components/Icon.astro';
-export { default as Icon } from './components/Icon.astro';
-export * from './components/Image.astro';
-export { default as Image } from './components/Image.astro';
-export * from './components/Input.astro';
-export { default as Input } from './components/Input.astro';
-export * from './components/Label.astro';
-export { default as Label } from './components/Label.astro';
-export * from './components/Layout.astro';
-export { default as Layout } from './components/Layout.astro';
-export * from './components/Link.astro';
-export { default as Link } from './components/Link.astro';
-export * from './components/Logo.astro';
-export { default as Logo } from './components/Logo.astro';
-export * from './components/Option.astro';
-export { default as Option } from './components/Option.astro';
-export * from './components/Page.astro';
-export { default as Page } from './components/Page.astro';
-export * from './components/Price.astro';
-export { default as Price } from './components/Price.astro';
-export * from './components/Prose.astro';
-export { default as Prose } from './components/Prose.astro';
-export * from './components/Rating.astro';
-export { default as Rating } from './components/Rating.astro';
-export * from './components/Section.astro';
-export { default as Section } from './components/Section.astro';
-export * from './components/Select.astro';
-export { default as Select } from './components/Select.astro';
-export * from './components/Stats.astro';
-export { default as Stats } from './components/Stats.astro';
-export * from './components/Tagline.astro';
-export { default as Tagline } from './components/Tagline.astro';
-export * from './components/Test.astro';
-export { default as Test } from './components/Test.astro';
-export * from './components/Textarea.astro';
-export { default as Textarea } from './components/Textarea.astro';
-export * from './components/Title.astro';
-export { default as Title } from './components/Title.astro';
-export * from './components/Video.astro';
-export { default as Video } from './components/Video.astro';
-export * from './components/Writeup.astro';
-export { default as Writeup } from './components/Writeup.astro';
-export * from './components/misc/Component.astro';
-export { default as Component } from './components/misc/Component.astro';
+export * from './components/Action.astro'
+export { default as Action } from './components/Action.astro'
+export * from './components/Badge.astro'
+export { default as Badge } from './components/Badge.astro'
+export * from './components/Button.astro'
+export { default as Button } from './components/Button.astro'
+export * from './components/Card.astro'
+export { default as Card } from './components/Card.astro'
+export * from './components/Checkbox.astro'
+export { default as Checkbox } from './components/Checkbox.astro'
+export * from './components/Composite.astro'
+export { default as Composite } from './components/Composite.astro'
+export * from './components/Deck.astro'
+export { default as Deck } from './components/Deck.astro'
+export * from './components/Description.astro'
+export { default as Description } from './components/Description.astro'
+export * from './components/Display.astro'
+export { default as Display } from './components/Display.astro'
+export * from './components/Field.astro'
+export { default as Field } from './components/Field.astro'
+export * from './components/Form.astro'
+export { default as Form } from './components/Form.astro'
+export * from './components/Head.astro'
+export { default as Head } from './components/Head.astro'
+export * from './components/Header.astro'
+export { default as Header } from './components/Header.astro'
+export * from './components/Icon.astro'
+export { default as Icon } from './components/Icon.astro'
+export * from './components/Image.astro'
+export { default as Image } from './components/Image.astro'
+export * from './components/Input.astro'
+export { default as Input } from './components/Input.astro'
+export * from './components/Label.astro'
+export { default as Label } from './components/Label.astro'
+export * from './components/Layout.astro'
+export { default as Layout } from './components/Layout.astro'
+export * from './components/Link.astro'
+export { default as Link } from './components/Link.astro'
+export * from './components/Logo.astro'
+export { default as Logo } from './components/Logo.astro'
+export * from './components/Option.astro'
+export { default as Option } from './components/Option.astro'
+export * from './components/Page.astro'
+export { default as Page } from './components/Page.astro'
+export * from './components/Price.astro'
+export { default as Price } from './components/Price.astro'
+export * from './components/Prose.astro'
+export { default as Prose } from './components/Prose.astro'
+export * from './components/Rating.astro'
+export { default as Rating } from './components/Rating.astro'
+export * from './components/Section.astro'
+export { default as Section } from './components/Section.astro'
+export * from './components/Select.astro'
+export { default as Select } from './components/Select.astro'
+export * from './components/Stats.astro'
+export { default as Stats } from './components/Stats.astro'
+export * from './components/Tagline.astro'
+export { default as Tagline } from './components/Tagline.astro'
+export * from './components/Test.astro'
+export { default as Test } from './components/Test.astro'
+export * from './components/Textarea.astro'
+export { default as Textarea } from './components/Textarea.astro'
+export * from './components/Title.astro'
+export { default as Title } from './components/Title.astro'
+export * from './components/Video.astro'
+export { default as Video } from './components/Video.astro'
+export * from './components/Writeup.astro'
+export { default as Writeup } from './components/Writeup.astro'
+export * from './components/misc/Component.astro'
+export { default as Component } from './components/misc/Component.astro'
