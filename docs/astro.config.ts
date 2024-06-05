0855f39c25e0f8621e7367644f1defc565d75f28
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-05 14:53:11 +0200

    chore: Update astro.config.ts for GitHub Pages deployment

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -6,8 +6,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fulldotdev.github.io/ui',
-  base: 'ui',
+  site: 'https://ui.full.dev',
   integrations: [
     starlight({
       title: 'Fulldev UI',
