75125f70a3fc4741c1b940a86de5a64c9ead3b6f
Sil <silveltman@gmail.com>
2024-06-14 20:47:19 +0200

    removed unneccesary packages

--- package/astro.config.mjs
+++ package/astro.config.mjs
@@ -1,7 +1,6 @@
-import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [],
 })
