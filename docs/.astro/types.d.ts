8887900c52938399cee0ea39979147d161a6428c
Sil <silveltman@gmail.com>
2024-06-27 11:37:29 +0200

    inputs partially cleaned up

--- docs/.astro/types.d.ts
+++ docs/.astro/types.d.ts
@@ -486,5 +486,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../src/content/config.js");
+	export type ContentConfig = typeof import("../src/content/config.js");
 }
