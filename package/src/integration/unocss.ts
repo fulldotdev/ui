10516fe619bdbca983250995eb89b8f270711772
Sil <silveltman@gmail.com>
2024-06-28 14:51:14 +0200

    uno simplified

--- package/src/integration/unocss.ts
+++ package/src/integration/unocss.ts
@@ -1,21 +1,26 @@
-import { definePreset } from 'unocss'
+import { definePreset, presetUno } from 'unocss'
 
 export default definePreset((options?: any) => {
   return {
     name: 'fullUI',
-    rules: [
-      ['.space-1', { margin: 'var(--space-1)' }],
-      ['.space-2', { margin: 'var(--space-2)' }],
-      ['.space-3', { margin: 'var(--space-3)' }],
-      ['.space-4', { margin: 'var(--space-4)' }],
-      ['.space-5', { margin: 'var(--space-5)' }],
-      ['.space-6', { margin: 'var(--space-6)' }],
-      ['.element-1', { padding: 'var(--element-1)' }],
-      ['.element-2', { padding: 'var(--element-2)' }],
-      ['.element-3', { padding: 'var(--element-3)' }],
-      ['.radius-1', { 'border-radius': 'var(--radius-1)' }],
-      ['.radius-2', { 'border-radius': 'var(--radius-2)' }],
-      ['.radius-3', { 'border-radius': 'var(--radius-3)' }],
-    ],
+    presets: [presetUno()],
+    theme: {
+      spacing: {
+        'space-1': 'var(--space-1)',
+        'space-2': 'var(--space-2)',
+        'space-3': 'var(--space-3)',
+        'space-4': 'var(--space-4)',
+        'space-5': 'var(--space-5)',
+        'space-6': 'var(--space-6)',
+        'element-1': 'var(--element-1)',
+        'element-2': 'var(--element-2)',
+        'element-3': 'var(--element-3)',
+      },
+      borderRadius: {
+        'radius-1': 'var(--radius-1)',
+        'radius-2': 'var(--radius-2)',
+        'radius-3': 'var(--radius-3)',
+      },
+    },
   }
 })
