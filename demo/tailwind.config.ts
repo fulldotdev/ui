6f87b21ec14ed77e7c9aedf22f2997be9bfdfeb3
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-28 11:21:50 +0200

    add background + border

--- demo/tailwind.config.ts
+++ demo/tailwind.config.ts
@@ -1,4 +1,4 @@
-import tailwindPlugin from '../package/src/integration/tailwind2.ts'
+import tailwindPlugin from '../package/src/integration/tailwind.ts'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
--- package/src/integration/tailwind.ts
+++ package/src/integration/tailwind.ts
@@ -1,29 +1,34 @@
 import plugin from 'tailwindcss/plugin'
 
-export const tailwindPlugin = plugin(
-  () => {
-    // Your plugin logic here
-  },
-  {
-    theme: {
-      extend: {
-        space: {
-          'space-1': 'var(--space-1)',
-          'space-2': 'var(--space-2)',
-          'space-3': 'var(--space-3)',
-          'space-4': 'var(--space-4)',
-          'space-5': 'var(--space-5)',
-          'space-6': 'var(--space-6)',
-          'element-1': 'var(--element-1)',
-          'element-2': 'var(--element-2)',
-          'element-3': 'var(--element-3)',
-        },
-        borderRadius: {
-          'radius-1': 'var(--radius-1)',
-          'radius-2': 'var(--radius-2)',
-          'radius-3': 'var(--radius-3)',
-        },
-      },
+export default plugin(() => {}, {
+  theme: {
+    spacing: {
+      'space-1': 'var(--space-1)',
+      'space-2': 'var(--space-2)',
+      'space-3': 'var(--space-3)',
+      'space-4': 'var(--space-4)',
+      'space-5': 'var(--space-5)',
+      'space-6': 'var(--space-6)',
+      'element-1': 'var(--element-1)',
+      'element-2': 'var(--element-2)',
+      'element-3': 'var(--element-3)',
     },
-  }
-)
+    borderRadius: {
+      'radius-1': 'var(--radius-1)',
+      'radius-2': 'var(--radius-2)',
+      'radius-3': 'var(--radius-3)',
+    },
+    background: {
+      'surface-1': 'var(--background-surface)',
+      subtle: 'var(--background-subtle)',
+      soft: 'var(--background-soft)',
+      hover: 'var(--background-hover)',
+      active: 'var(--background-active)',
+    },
+    border: {
+      default: '1px solid var(--border)',
+      outline: '1px solid var(--border-outline)',
+      hover: '1px solid var(--border-hover)',
+    },
+  },
+})
