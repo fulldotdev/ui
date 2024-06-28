36d5b9085b749a4fc9eba0ecf2dc9bc74db7920b
Jeremy Bosma <jectaunscripted@gmail.com>
2024-06-28 10:15:20 +0200

    tailwind plugin

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -1,13 +1,8 @@
 // @ts-ignore
-import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind()],
 })
--- package/src/integration/tailwind.ts
+++ package/src/integration/tailwind.ts
@@ -1,28 +1,31 @@
-import plugin from 'tailwindcss/plugin'
+const plugin = require('tailwindcss/plugin')
 
-export const tailwindPlugin = plugin(
-  () => {
-    // Your plugin logic here
-  },
-  {
-    theme: {
-      spacing: {
-        'space-1': 'var(--space-1)',
-        'space-2': 'var(--space-2)',
-        'space-3': 'var(--space-3)',
-        'space-4': 'var(--space-4)',
-        'space-5': 'var(--space-5)',
-        'space-6': 'var(--space-6)',
+const tailwindPlugin = plugin(function ({
+  addUtilities,
+}: {
+  addUtilities: any
+}) {
+  const newUtilities = {
+    '.space-test': {
+      margin: 'var(--space-1)',
+    },
 
-        'element-1': 'var(--element-1)',
-        'element-2': 'var(--element-2)',
-        'element-3': 'var(--element-3)',
-      },
-      borderRadius: {
-        'radius-1': 'var(--radius-1)',
-        'radius-2': 'var(--radius-2)',
-        'radius-3': 'var(--radius-3)',
-      },
+    '.rounded-1': {
+      borderRadius: 'var(--radius-1)',
+    },
+    '.rounded-2': {
+      borderRadius: 'var(--radius-2)',
+    },
+    '.rounded-3': {
+      borderRadius: 'var(--radius-3)',
+    },
+
+    '.background': {
+      backgroundColor: 'var(--background)',
     },
   }
-)
+
+  addUtilities(newUtilities)
+})
+
+module.exports = tailwindPlugin
