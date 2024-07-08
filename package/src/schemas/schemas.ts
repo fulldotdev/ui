c81cfe985f43f4aad316475ebabbd290f2897c86
Sil <silveltman@gmail.com>
2024-07-08 12:59:02 +0200

    all schemas partial passthrough

--- package/src/schemas/schemas.ts
+++ package/src/schemas/schemas.ts
@@ -6,40 +6,52 @@ export const pathSchema = z.string().transform((value) => {
   return slug
 })
 
-export const buttonSchema = z.object({
-  text: z.string(),
-  html: z.string(),
-  href: z.string(),
-})
+export const buttonSchema = z
+  .object({
+    text: z.string(),
+    html: z.string(),
+    href: z.string(),
+  })
+  .partial()
+  .passthrough()
 
-const baseSchema = z.object({
-  image: z.string(),
-  images: z.string().array(),
-  icon: z.string(),
-  rating: z.number(),
-  badge: z.string(),
-  tagline: z.string(),
-  heading: z.string(),
-  text: z.string(),
-  list: z.string().array(),
-  price: z.string().or(z.number()),
-  button: buttonSchema,
-  buttons: buttonSchema.array(),
-})
+const baseSchema = z
+  .object({
+    image: z.string(),
+    images: z.string().array(),
+    icon: z.string(),
+    rating: z.number(),
+    badge: z.string(),
+    tagline: z.string(),
+    heading: z.string(),
+    text: z.string(),
+    list: z.string().array(),
+    price: z.string().or(z.number()),
+    button: buttonSchema,
+    buttons: buttonSchema.array(),
+  })
+  .partial()
+  .passthrough()
 
-export const blockSchema = baseSchema.extend({
-  card: baseSchema,
-  cards: baseSchema.array(),
-})
+export const blockSchema = baseSchema
+  .extend({
+    card: baseSchema,
+    cards: baseSchema.array(),
+  })
+  .partial()
+  .passthrough()
 
-export const pageSchema = baseSchema.extend({
-  title: z.string(),
-  description: z.string(),
-  head: z
-    .object({
-      title: z.string(),
-      description: z.string(),
-      image: z.string().optional(),
-    })
-    .partial(),
-})
+export const pageSchema = baseSchema
+  .extend({
+    title: z.string(),
+    description: z.string(),
+    head: z
+      .object({
+        title: z.string(),
+        description: z.string(),
+        image: z.string().optional(),
+      })
+      .partial(),
+  })
+  .partial()
+  .passthrough()
