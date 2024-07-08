390eb388760ef48d8870348026438e2a22c7aa76
Sil <silveltman@gmail.com>
2024-07-08 13:39:50 +0200

    changes table imports to relative

--- package/src/content/config.ts
+++ package/src/content/config.ts
@@ -1,14 +1,14 @@
-import { defineCollection, reference } from 'astro:content'
-import { pageSchema } from '../schemas/schemas'
+// import { defineCollection, reference } from 'astro:content'
+// import { pageSchema } from '../schemas/schemas'
 
-const collection = defineCollection({
-  type: 'content',
-  schema: pageSchema({
-    page: reference('pages'),
-    pages: reference('pages').array(),
-  }),
-})
+// const collection = defineCollection({
+//   type: 'content',
+//   schema: pageSchema({
+//     page: reference('pages'),
+//     pages: reference('pages').array(),
+//   }),
+// })
 
-export const collections = {
-  pages: collection,
-}
+// export const collections = {
+//   pages: collection,
+// }
