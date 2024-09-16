47a0216ea7092094e7757aa137417490b4aa962b
Sil <silveltman@gmail.com>
2024-09-16 13:40:12 +0200

    ts errors in schemas fixed

--- src/schemas/block.ts
+++ src/schemas/block.ts
@@ -6,21 +6,10 @@ import { pathSchema } from './utils'
 export const block = base
   .extend({
     columns: card.array(),
-    cards: z.array(card.or(pathSchema('cards'))),
-    source: z.literal('children').or(z.literal('references')),
+    cards: z.array(card),
+    source: z.enum(['children', 'references']),
     pages: pathSchema('pages').array(),
     records: pathSchema('records').array(),
-    page: pathSchema('pages'),
-    block: pathSchema('blocks'),
-    posts: pathSchema('posts').array(),
-    reviews: pathSchema('reviews').array(),
-    review: base,
-    features: base.array(),
   })
   .partial()
   .passthrough()
-
-// export type Block<As extends HTMLTag> = z.infer<typeof base> &
-//   ComponentProps<typeof Section<As>> &
-//   Pick<ComponentProps<typeof Heading>, 'level'> &
-//   Pick<ComponentProps<typeof Image>, 'position'>
