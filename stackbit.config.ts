82842b019fe02fa88bf91a79e785eabd2c73815a
Sil <silveltman@gmail.com>
2024-09-24 10:24:54 +0200

    removed automatically added stackbit file

--- stackbit.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineStackbitConfig } from '@stackbit/types';
-
-export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "astro",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
