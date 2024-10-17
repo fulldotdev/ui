6e40f8508656f16fdfcc24ed1ed3b9aad353a244
Sil <silveltman@gmail.com>
2024-10-17 17:01:07 +0200

    allmost all types simplified, and no passtrough/catchall any

---
+++ src/types/global-props.d.ts
@@ -0,0 +1,16 @@
+export type GlobalProps = {
+  if?: any
+  theme?: 'light' | 'dark' | undefined
+  color?: 'base' | 'brand' | string | null | undefined
+  size?: 'sm' | 'md' | 'lg' | string | number | null | undefined
+  contrast?: boolean | undefined
+}
+
+// declare global {
+//   type GlobalProps = {
+//     theme?: 'light' | 'dark' | undefined
+//     color?: 'base' | 'brand' | undefined
+//     size?: 'sm' | 'md' | 'lg' | undefined
+//     contrast?: boolean | undefined
+//   }
+// }
