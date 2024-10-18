a813c743d7c38cbca19bc13b4d0d43b29775314b
Sil <silveltman@gmail.com>
2024-10-18 13:26:56 +0200

    getEntry function moved out of Button

--- src/schemas/components/button.ts
+++ src/schemas/components/button.ts
@@ -1,7 +1,38 @@
-import { z } from 'astro:content'
+import { getEntry, z } from 'astro:content'
 import pathSchema from '../utils/pathSchema'
 
-export default pathSchema('pages').or(
+const button = z
+  .object({
+    text: z.string(),
+    href: z.string(),
+    icon: z.string(),
+  })
+  .partial()
+  .passthrough()
+
+// export default button.or(
+//   pathSchema('pages').transform(async (value) => {
+//     const entry = (await getEntry(value)) as any
+//     console.log(entry)
+//     return {
+//       text: entry.data.title,
+//       href: `/${entry.slug}/`,
+//     }
+//   })
+// )
+
+export default z.preprocess(
+  async (value) => {
+    if (typeof value === 'string') {
+      const reference = await pathSchema('pages').parseAsync(value)
+      const entry = (await getEntry(reference)) as any
+      const object = {
+        text: entry.data.title,
+        href: `/${entry.slug}/`,
+      }
+      return object
+    } else return value
+  },
   z
     .object({
       text: z.string(),
