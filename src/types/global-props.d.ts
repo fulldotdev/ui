82b1e874e202be33a080bbaa913a405100822f77
Sil <silveltman@gmail.com>
2024-10-19 18:59:53 +0200

    badge based on button, badges plural, logo now has different props for company name/image, Section spread fix, button/badge transition

--- src/types/global-props.d.ts
+++ src/types/global-props.d.ts
@@ -1,6 +1,6 @@
 export type GlobalProps = {
   if?: any
-  theme?: 'light' | 'dark' | undefined
+  theme?: 'light' | 'dark' | string | null | undefined
   color?: 'base' | 'brand' | string | null | undefined
   size?: 'sm' | 'md' | 'lg' | string | number | null | undefined
   contrast?: boolean | undefined
