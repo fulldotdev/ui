c0d2a17942271b20dabe78be2bed497e49e2e03f
Sil <silveltman@gmail.com>
2024-10-22 13:21:25 +0200

    preset collection now uses pageschema

--- src/schemas/pageSchema.ts
+++ src/schemas/pageSchema.ts
@@ -1,7 +1,15 @@
 import { getEntries, getEntry, z } from 'astro:content'
 import { assign } from 'radash'
 import fulldevConfig from 'virtual:fulldev-ui/config'
-import presetSchema from './presetSchema'
+import footer from './components/footer'
+import header from './components/header'
+import image from './components/image'
+import sections from './components/sections'
+import sidebar from './components/sidebar'
+import toc from './components/toc'
+import component from './utils/component'
+import navigation from './utils/navigation'
+import parents from './utils/parents'
 import pathSchema from './utils/pathSchema'
 
 const i18n = z
@@ -40,4 +48,29 @@ const presets = z
     return mergedData as typeof rest
   })
 
-export default i18n.pipe(presets).pipe(presetSchema)
+export default i18n.pipe(presets).pipe(
+  z
+    .object({
+      component,
+      parents,
+      theme: z.enum(['light', 'dark']),
+      lang: z.string(),
+      seo: z
+        .object({
+          title: z.string().optional(),
+          description: z.string().optional(),
+        })
+        .optional(),
+      title: z.string(),
+      description: z.string().optional(),
+      image,
+      sections,
+      footer,
+      header: z.boolean().or(header),
+      sidebar: z.boolean().or(sidebar),
+      navigation,
+      toc,
+    })
+    .partial()
+    .passthrough()
+)
