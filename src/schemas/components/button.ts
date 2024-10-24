59cb21b9cf4ab9c5c07d4527fcbe8c46c2b0e3a3
Sil <silveltman@gmail.com>
2024-10-24 15:10:25 +0200

    c

--- src/schemas/components/button.ts
+++ src/schemas/components/button.ts
@@ -1,16 +1,39 @@
+// import { z } from 'astro:content'
+// import pathSchema from '../utils/pathSchema'
+
+// export default z.union([pathSchema('pages'), z.object({}).passthrough()]).pipe(
+//   z
+//     .object({
+//       slug: z
+//         .string()
+//         .refine(async (data) => await pathSchema('pages').parseAsync(data)),
+//       text: z.string(),
+//       href: z.string(),
+//       icon: z.string(),
+//     })
+//     .partial()
+//     .passthrough()
+// )
+
 import { z } from 'astro:content'
 import pathSchema from '../utils/pathSchema'
 
-export default z.union([pathSchema('pages'), z.object({}).passthrough()]).pipe(
-  z
-    .object({
-      slug: z
-        .string()
-        .refine(async (data) => await pathSchema('pages').parseAsync(data)),
-      text: z.string(),
-      href: z.string(),
-      icon: z.string(),
-    })
-    .partial()
-    .passthrough()
-)
+export const buttonSchema = z
+  .union([pathSchema('pages'), z.object({}).passthrough()])
+  .pipe(
+    z
+      .object({
+        slug: z
+          .string()
+          .refine(async (data) => await pathSchema('pages').parseAsync(data)),
+        text: z.string(),
+        href: z.string(),
+        icon: z.string(),
+      })
+      .partial()
+      .passthrough()
+  )
+
+export type ButtonSchema = z.infer<typeof buttonSchema>
+
+export default buttonSchema
