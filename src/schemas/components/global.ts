cfd87261148d10fc17de9d6f5374ff68e26e165b
Sil <silveltman@gmail.com>
2024-10-25 11:41:04 +0200

    fixes

--- src/schemas/components/global.ts
+++ src/schemas/components/global.ts
@@ -3,10 +3,10 @@ import { z } from 'astro:content'
 export const globalSchema = z
   .object({
     if: z.any(),
-    theme: z.enum(['light', 'dark']).or(z.string()),
-    color: z.enum(['base', 'brand']).or(z.string()),
-    size: z.enum(['sm', 'md', 'lg']).or(z.string()).or(z.number()),
-    contrast: z.boolean().optional(),
+    theme: z.enum(['light', 'dark']),
+    color: z.enum(['base', 'brand']),
+    size: z.enum(['sm', 'md', 'lg']),
+    contrast: z.boolean(),
   })
   .partial()
   .passthrough()
