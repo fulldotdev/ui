eb3d440d9a918771a75b2326c0b0b9057c292ce8
Sil <silveltman@gmail.com>
2024-12-10 21:19:33 +0100

    more cleanup and build working local

--- astro.config.ts
+++ astro.config.ts
@@ -4,21 +4,19 @@ import integration from 'fulldev-ui/integration'
 
 // https://astro.build/config
 export default defineConfig({
-  publicDir: './src/images',
   devToolbar: {
     enabled: false,
   },
   integrations: [
     integration({
       company: 'Fulldev UI',
-      favicon: 'src/images/favicon.svg',
+      favicon: 'public/images/favicon.svg',
       css: '/src/css/custom.css',
       injectRoutes: false,
     }),
     svelte(),
   ],
   redirects: {
-    '/discord': 'https://discord.gg/vXZqMbadm8',
     // '/structure/[...slug]': '/docs/components/[...slug]',
     // '/segment/[...slug]': '/docs/components/[...slug]',
     // '/typography/[...slug]': '/docs/components/[...slug]',
