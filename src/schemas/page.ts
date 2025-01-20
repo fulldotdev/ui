425d876c7faa20355c1a0f1863518774841d302a
Sil <silveltman@gmail.com>
2025-01-20 22:35:28 +0100

    simplified page schema

--- src/schemas/page.ts
+++ src/schemas/page.ts
@@ -1,28 +1,15 @@
 import { z } from 'astro:content'
 import { blockSchema } from 'fulldev-ui/schemas/block.ts'
 import { buttonSchema } from 'fulldev-ui/schemas/button.ts'
-import { channelSchema } from 'fulldev-ui/schemas/channel.ts'
 import { formSchema } from 'fulldev-ui/schemas/form.ts'
 import { headSchema } from 'fulldev-ui/schemas/head.ts'
 import { imageSchema } from 'fulldev-ui/schemas/image.ts'
-import { logoSchema } from 'fulldev-ui/schemas/logo.ts'
-import { menuSchema } from 'fulldev-ui/schemas/menu.ts'
 import { pathSchema } from 'fulldev-ui/schemas/path.ts'
 
 export const pageSchema = z
   .object({
-    // Config
     _schema: z.string().optional(),
-    block: z.string().optional(),
-    variant: z.number().optional(),
     slug: z.string().optional(),
-    // Globals
-    logo: logoSchema.optional(),
-    socials: z.string().array().optional(),
-    channels: channelSchema.array().optional(),
-    menus: menuSchema.array().optional(),
-    // Block
-    tagline: z.string().optional(),
     title: z.string().optional(),
     description: z.string().optional(),
     list: z.string().array().optional(),
@@ -35,11 +22,8 @@ export const pageSchema = z
     variants: z.record(z.string(), z.string().array()).optional(),
     categories: pathSchema('pages').array().optional(),
     form: formSchema.optional(),
-    // Page
-    head: headSchema.optional(),
-    header: blockSchema.optional(),
     sections: blockSchema.array().optional(),
-    footer: blockSchema.optional(),
+    head: headSchema.optional(),
   })
   .strict()
 
