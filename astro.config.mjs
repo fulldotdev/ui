3fbf0ffdbcab8565a96a10acf96d646c0be927d0
Sil <silveltman@gmail.com>
2025-02-05 15:26:08 +0100

    blocks improved

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import { defineConfig } from 'astro/config'
 import fulldev from 'fulldev-ui/integration'
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [fulldev({})],
+  integrations: [fulldev({}), vue()],
 })
