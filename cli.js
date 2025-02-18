0226b12c149b25f8769d276b3546765e37680c3a
Sil <silveltman@gmail.com>
2025-02-18 13:17:40 +0100

    cli improved and stackbit installed

--- cli.js
+++ cli.js
@@ -2,11 +2,14 @@
 import fs from 'fs-extra'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+console.log('running fulldev-ui cli...')
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// Items to copy with source and target paths
-const items = [
+const components = [
   {
     source: './src/blocks',
     target: 'src/blocks',
@@ -37,15 +40,56 @@ const items = [
   },
 ]
 
-items.forEach((item) => {
-  const sourcePath = path.join(__dirname, item.source)
-  const targetPath = path.join(process.cwd(), item.target)
+const content = [
+  {
+    source: './netlify',
+    target: 'netlify',
+  },
+  {
+    source: './src/content.config.ts',
+    target: 'src/content.config.ts',
+  },
+  {
+    source: './stackbit',
+    target: 'stackbit',
+  },
+  {
+    source: './netlify.toml',
+    target: 'netlify.toml',
+  },
+  {
+    source: './renovate.json',
+    target: 'renovate.json',
+  },
+  {
+    source: './stackbit.config.ts',
+    target: 'stackbit.config.ts',
+  },
+]
 
-  // Create parent directory for files
-  const targetDir = fs.statSync(sourcePath).isDirectory() ? targetPath : path.dirname(targetPath)
-  fs.ensureDirSync(targetDir)
+function copyItems(items) {
+  items.forEach((item) => {
+    const sourcePath = path.join(__dirname, item.source)
+    const targetPath = path.join(process.cwd(), item.target)
 
-  // Copy item
-  fs.copySync(sourcePath, targetPath)
-  console.log(`${item.target} copied successfully!`)
-})
+    // Create parent directory for files
+    const targetDir = fs.statSync(sourcePath).isDirectory() ? targetPath : path.dirname(targetPath)
+    fs.ensureDirSync(targetDir)
+
+    // Copy item
+    fs.copySync(sourcePath, targetPath)
+    console.log(`${item.target} copied successfully!`)
+  })
+}
+
+yargs(hideBin(process.argv))
+  .command('components', 'Copy components and related files', () => {
+    console.log('copying components...')
+    copyItems(components)
+  })
+  .command('content', 'Copy content and related files', () => {
+    console.log('copying content...')
+    copyItems(content)
+  })
+  .demandCommand(1, 'You need to specify either "components" or "content"')
+  .parse()
