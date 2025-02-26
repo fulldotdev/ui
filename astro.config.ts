28f254927c36ef31a941c346b66e01c012473daf
Sil <silveltman@gmail.com>
2025-02-26 19:29:14 +0100

    shadcn with tailwind v4

--- astro.config.ts
+++ astro.config.ts
@@ -1,26 +1,20 @@
+import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
-
-import react from '@astrojs/react'
-
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://ui.full.dev',
   trailingSlash: 'always',
-
   prefetch: {
     prefetchAll: true,
   },
-
-  integrations: [robotsTxt(), sitemap(), vue(), react()],
-
   devToolbar: {
     enabled: false,
   },
-
+  integrations: [robotsTxt(), sitemap(), vue(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
