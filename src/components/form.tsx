b7b5e6c4218653cf7e2f00564329f03478f47ede
Sil <silveltman@gmail.com>
2025-04-23 12:47:51 +0200

    Update Form component: Add name attribute to Input field for improved accessibility and form handling.

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -103,6 +103,7 @@ function Form({ fields, submit, className, ...props }: Props) {
                               <PopoverTrigger asChild>
                                 <FormControl>
                                   <Input
+                                    name={name || label}
                                     className="text-left"
                                     placeholder={placeholder}
                                     value={
