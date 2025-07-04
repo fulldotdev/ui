2f0279c93a863ca6a705f16ea86679f45da51ba7
Sil <silveltman@gmail.com>
2025-07-04 22:23:00 +0200

    Enhance FAQ block and content: Adjust margin for Accordion component in faqs-1.tsx, update AccordionItem key format for better accessibility, and add new content files for FAQs and general content with structured data. Remove unused links from site.json for cleaner navigation.

--- src/blocks/faqs-1.tsx
+++ src/blocks/faqs-1.tsx
@@ -17,12 +17,12 @@ export default function ({ children, items }: BlockProps) {
       <SectionContainer className="max-w-screen-md lg:px-12">
         {children && <SectionContent>{children}</SectionContent>}
         <Accordion
-          className="w-full max-w-2xl not-first:mt-16"
+          className="w-full max-w-2xl not-first:mt-12"
           type="single"
           collapsible
         >
           {items?.map(({ title, description }, i) => (
-            <AccordionItem key={i} value={title ?? ""}>
+            <AccordionItem key={i} value={`${i}. ${title}`}>
               <AccordionTrigger>{title}</AccordionTrigger>
               <AccordionContent>{description}</AccordionContent>
             </AccordionItem>
---
+++ src/content/blocks/faqs.md
@@ -0,0 +1,14 @@
+---
+children: >-
+  <h2>Lorem ipsum dolor sit amet</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+items:
+  - title: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+  - title: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+  - title: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+  - title: "Lorem ipsum dolor sit amet"
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+---
