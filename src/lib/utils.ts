fdd510693dcada2e21ccad99a7c46ccc75450a2d
Sil <silveltman@gmail.com>
2025-08-05 22:20:40 +0200

    Remove unused money and discount utility functions from utils.ts to enhance code clarity and maintainability.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,18 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function money(
-  amount: number,
-  currency: string = "EUR",
-  locale: string = "nl-NL"
-) {
-  return new Intl.NumberFormat(locale, {
-    style: "currency",
-    currency: currency,
-  }).format(amount)
-}
-
-export function discount(before: number, after: number) {
-  return `-${Math.round(((before - after) / before) * 100)}%`
-}
