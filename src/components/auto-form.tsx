f1a9e0ec9ff2019fdb8acc1a6023b30b1fc3ae0a
Sil <silveltman@gmail.com>
2025-08-27 18:44:10 +0200

    auto-form big improvements

--- src/components/auto-form.tsx
+++ src/components/auto-form.tsx
@@ -1,7 +1,11 @@
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
 import { useForm } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Checkbox } from "@/components/ui/checkbox"
 import {
   FormControl,
   FormDescription,
@@ -12,6 +16,11 @@ import {
   Form as FormRoot,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import {
   Select,
@@ -20,6 +29,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Slider } from "@/components/ui/slider"
 import { Textarea } from "@/components/ui/textarea"
 
 function AutoFormInput({
@@ -153,7 +163,7 @@ function AutoFormSelect({
   )
 }
 
-function AutoFormRadioGroup({
+function AutoFormRadio({
   control,
   name,
   label,
@@ -179,14 +189,13 @@ function AutoFormRadioGroup({
             <RadioGroup
               onValueChange={field.onChange}
               defaultValue={field.value}
-              className="flex flex-col space-y-2"
+              className="flex flex-col"
               disabled={disabled}
+              required={required}
+              name={name}
             >
               {options?.map((option) => (
-                <FormItem
-                  key={option}
-                  className="flex items-center space-y-0 space-x-3"
-                >
+                <FormItem key={option} className="flex items-center gap-3">
                   <FormControl>
                     <RadioGroupItem value={option} />
                   </FormControl>
@@ -197,6 +206,282 @@ function AutoFormRadioGroup({
           </FormControl>
           <FormDescription>{description}</FormDescription>
           <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function AutoFormCheckbox({
+  control,
+  name,
+  label,
+  description,
+  required,
+  disabled,
+}: Pick<React.ComponentProps<typeof FormField>, "control" | "name"> & {
+  label?: string
+  description?: string
+  required?: boolean
+  disabled?: boolean
+}) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-row items-start gap-3">
+          <FormControl>
+            <Checkbox
+              checked={field.value}
+              onCheckedChange={field.onChange}
+              disabled={disabled}
+              required={required}
+              name={name}
+            />
+          </FormControl>
+          <div className="space-y-1 leading-none">
+            <FormLabel>{label}</FormLabel>
+            {description && <FormDescription>{description}</FormDescription>}
+          </div>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function AutoFormChoice({
+  control,
+  name,
+  label,
+  description,
+  options = [],
+  required,
+  disabled,
+}: Pick<React.ComponentProps<typeof FormField>, "control" | "name"> & {
+  label?: string
+  description?: string
+  options?: string[]
+  required?: boolean
+  disabled?: boolean
+}) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2"
+              disabled={disabled}
+              required={required}
+              name={name}
+            >
+              {options.map((option) => (
+                <FormLabel
+                  key={option}
+                  htmlFor={`${name}-${option}`}
+                  className="hover:bg-accent/50 has-[[aria-checked=true]]:border-ring has-[[aria-checked=true]]:bg-ring/5 flex items-start gap-3 rounded-lg border p-3"
+                >
+                  <RadioGroupItem value={option} id={`${name}-${option}`} />
+                  <div className="grid gap-1.5 font-normal">
+                    <p className="text-sm leading-none font-medium">{option}</p>
+                  </div>
+                </FormLabel>
+              ))}
+            </RadioGroup>
+          </FormControl>
+          <FormDescription>{description}</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function AutoFormMultiChoice({
+  control,
+  name,
+  label,
+  description,
+  options = [],
+  required,
+  disabled,
+}: Pick<React.ComponentProps<typeof FormField>, "control" | "name"> & {
+  label?: string
+  description?: string
+  options?: string[]
+  required?: boolean
+  disabled?: boolean
+}) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2">
+              {options.map((option) => (
+                <FormLabel
+                  key={option}
+                  htmlFor={`${name}-${option}`}
+                  className="hover:bg-accent/50 has-[[aria-checked=true]]:border-ring has-[[aria-checked=true]]:bg-ring/5 flex items-start gap-3 rounded-lg border p-3"
+                >
+                  <Checkbox
+                    id={`${name}-${option}`}
+                    checked={field.value?.includes(option)}
+                    onCheckedChange={(checked) => {
+                      return checked
+                        ? field.onChange([...(field.value || []), option])
+                        : field.onChange(
+                            field.value?.filter(
+                              (value: string) => value !== option
+                            )
+                          )
+                    }}
+                    disabled={disabled}
+                    required={required}
+                    name={name}
+                  />
+                  <div className="grid gap-1.5 font-normal">
+                    <p className="text-sm leading-none font-medium">{option}</p>
+                  </div>
+                </FormLabel>
+              ))}
+            </div>
+          </FormControl>
+          <FormDescription>{description}</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function AutoFormSlider({
+  control,
+  name,
+  label,
+  description,
+  min = 0,
+  max = 100,
+  step = 1,
+  defaultValue = (min + max) / 2,
+  required,
+  disabled,
+}: Pick<React.ComponentProps<typeof FormField>, "control" | "name"> &
+  Pick<React.ComponentProps<typeof Slider>, "min" | "max" | "step"> & {
+    label?: string
+    description?: string
+    defaultValue?: number
+    required?: boolean
+    disabled?: boolean
+  }) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>
+            {label} ({field.value ?? defaultValue})
+          </FormLabel>
+          <FormControl>
+            <Slider
+              min={min}
+              max={max}
+              step={step}
+              value={[field.value ?? defaultValue]}
+              onValueChange={(value) => field.onChange(value[0])}
+              className="w-full"
+              disabled={disabled}
+            />
+          </FormControl>
+          <FormDescription>{description}</FormDescription>
+          <FormMessage />
+          <input
+            className="pointer-events-none absolute opacity-0"
+            type="number"
+            name={name}
+            required={required}
+            min={min}
+            max={max}
+            step={step}
+            value={field.value ?? defaultValue}
+          />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+function AutoFormDate({
+  control,
+  name,
+  label,
+  description,
+  placeholder = "Pick a date",
+  required,
+  disabled,
+}: Pick<React.ComponentProps<typeof FormField>, "control" | "name"> & {
+  label?: string
+  description?: string
+  placeholder?: string
+  required?: boolean
+  disabled?: boolean
+}) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col">
+          <FormLabel>{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-full pl-3 text-left font-normal",
+                    !field.value && "text-muted-foreground"
+                  )}
+                  disabled={disabled}
+                >
+                  {field.value ? (
+                    format(field.value, "PPP")
+                  ) : (
+                    <span>{placeholder}</span>
+                  )}
+                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={field.value}
+                onSelect={field.onChange}
+                required={required}
+              />
+            </PopoverContent>
+          </Popover>
+          <FormDescription>{description}</FormDescription>
+          <FormMessage />
+          <input
+            className="pointer-events-none absolute opacity-0"
+            type="date"
+            required={required}
+            name={name}
+            value={field.value}
+          />
         </FormItem>
       )}
     />
@@ -217,7 +502,22 @@ interface Props extends React.ComponentProps<"form"> {
       } & React.ComponentProps<typeof AutoFormSelect>)
     | ({
         type: "radio"
-      } & React.ComponentProps<typeof AutoFormRadioGroup>)
+      } & React.ComponentProps<typeof AutoFormRadio>)
+    | ({
+        type: "checkbox"
+      } & React.ComponentProps<typeof AutoFormCheckbox>)
+    | ({
+        type: "choice"
+      } & React.ComponentProps<typeof AutoFormChoice>)
+    | ({
+        type: "multichoice"
+      } & React.ComponentProps<typeof AutoFormMultiChoice>)
+    | ({
+        type: "slider"
+      } & React.ComponentProps<typeof AutoFormSlider>)
+    | ({
+        type: "date"
+      } & React.ComponentProps<typeof AutoFormDate>)
   )[]
   submit?: string
   description?: string
@@ -242,15 +542,15 @@ function AutoForm({
       >
         {/* Cloudcannon fields */}
         {inbox && <input type="hidden" name="inbox_key" defaultValue={inbox} />}
-        <input type="text" name="_gotcha" style={{ display: "none" }} />
+        <input type="text" name="_gotcha" className="hidden" />
         {/* Always include the page path in the form data */}
         <input
           type="text"
           name="Pagina"
+          className="hidden"
           defaultValue={
             typeof window !== "undefined" ? window.location.pathname : ""
           }
-          style={{ display: "none" }}
         />
         {/* Fields definitions */}
         {fields?.map(({ type, ...field }, i) => {
@@ -261,7 +561,12 @@ function AutoForm({
             number: AutoFormInput,
             textarea: AutoFormTextarea,
             select: AutoFormSelect,
-            radio: AutoFormRadioGroup,
+            radio: AutoFormRadio,
+            checkbox: AutoFormCheckbox,
+            choice: AutoFormChoice,
+            multichoice: AutoFormMultiChoice,
+            slider: AutoFormSlider,
+            date: AutoFormDate,
           }
           const Field = fieldComponents[type]
           return <Field key={i} control={form.control} {...field} />
@@ -277,4 +582,15 @@ function AutoForm({
   )
 }
 
-export { AutoFormInput, AutoFormTextarea, AutoFormSelect, AutoForm }
+export {
+  AutoFormInput,
+  AutoFormTextarea,
+  AutoFormSelect,
+  AutoFormRadio,
+  AutoFormDate,
+  AutoFormSlider,
+  AutoFormCheckbox,
+  AutoFormChoice,
+  AutoFormMultiChoice,
+  AutoForm,
+}
