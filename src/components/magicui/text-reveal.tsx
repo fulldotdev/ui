8479faaf2f62759b4482c2993ee38976bca96a36
Sil <silveltman@gmail.com>
2025-09-05 12:53:42 +0200

    added missing components

---
+++ src/components/magicui/text-reveal.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import {
+  useRef,
+  type ComponentPropsWithoutRef,
+  type FC,
+  type ReactNode,
+} from "react"
+import { motion, useScroll, useTransform, type MotionValue } from "motion/react"
+
+import { cn } from "@/lib/utils"
+
+export interface TextRevealProps extends ComponentPropsWithoutRef<"div"> {
+  children: string
+}
+
+export const TextReveal: FC<TextRevealProps> = ({ children, className }) => {
+  const targetRef = useRef<HTMLDivElement | null>(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  })
+
+  if (typeof children !== "string") {
+    throw new Error("TextReveal: children must be a string")
+  }
+
+  const words = children.split(" ")
+
+  return (
+    <div ref={targetRef} className={cn("relative z-0 h-[200vh]", className)}>
+      <div
+        className={
+          "sticky top-0 mx-auto flex h-[50%] max-w-4xl items-center bg-transparent px-[1rem] py-[5rem]"
+        }
+      >
+        <span
+          ref={targetRef}
+          className={
+            "flex flex-wrap p-5 text-2xl font-bold text-black/20 md:p-8 md:text-3xl lg:p-10 lg:text-4xl xl:text-5xl dark:text-white/20"
+          }
+        >
+          {words.map((word, i) => {
+            const start = i / words.length
+            const end = start + 1 / words.length
+            return (
+              <Word key={i} progress={scrollYProgress} range={[start, end]}>
+                {word}
+              </Word>
+            )
+          })}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+interface WordProps {
+  children: ReactNode
+  progress: MotionValue<number>
+  range: [number, number]
+}
+
+const Word: FC<WordProps> = ({ children, progress, range }) => {
+  const opacity = useTransform(progress, range, [0, 1])
+  return (
+    <span className="xl:lg-3 relative mx-1 lg:mx-1.5">
+      <span className="absolute opacity-30">{children}</span>
+      <motion.span
+        style={{ opacity: opacity }}
+        className={"text-black dark:text-white"}
+      >
+        {children}
+      </motion.span>
+    </span>
+  )
+}
