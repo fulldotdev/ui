090387335d0cc80c7c52322d2025f85b3480f0e3
Sil <silveltman@gmail.com>
2025-09-23 17:45:12 +0200

    Implement section component to replace block component in layout and pages, enhancing modularity. Update registry and schemas to reflect new structure. Add slot support to various block components for improved flexibility.

---
+++ src/schemas/section.ts
@@ -0,0 +1,75 @@
+import { z } from "astro:schema"
+
+import { articlesSchema } from "@/schemas/blocks/articles"
+import { blocksSchema } from "@/schemas/blocks/blocks"
+import { collectionsSchema } from "@/schemas/blocks/collections"
+import { contactSchema } from "@/schemas/blocks/contact"
+import { contentSchema } from "@/schemas/blocks/content"
+import { ctaSchema } from "@/schemas/blocks/cta"
+import { faqsSchema } from "@/schemas/blocks/faqs"
+import { featureSchema } from "@/schemas/blocks/feature"
+import { featuresSchema } from "@/schemas/blocks/features"
+import { jobsSchema } from "@/schemas/blocks/jobs"
+import { personsSchema } from "@/schemas/blocks/persons"
+import { pricingsSchema } from "@/schemas/blocks/pricings"
+import { productsSchema } from "@/schemas/blocks/products"
+import { quoteSchema } from "@/schemas/blocks/quote"
+import { reviewSchema } from "@/schemas/blocks/review"
+import { reviewsSchema } from "@/schemas/blocks/reviews"
+import { servicesSchema } from "@/schemas/blocks/services"
+
+export const sectionSchema = z.discriminatedUnion("type", [
+  articlesSchema.extend({
+    type: z.literal("articles"),
+  }),
+  blocksSchema.extend({
+    type: z.literal("blocks"),
+  }),
+  collectionsSchema.extend({
+    type: z.literal("collections"),
+  }),
+  contactSchema.extend({
+    type: z.literal("contact"),
+  }),
+  contentSchema.extend({
+    type: z.literal("content"),
+  }),
+  ctaSchema.extend({
+    type: z.literal("cta"),
+  }),
+  faqsSchema.extend({
+    type: z.literal("faqs"),
+  }),
+  featureSchema.extend({
+    type: z.literal("feature"),
+  }),
+  featuresSchema.extend({
+    type: z.literal("features"),
+  }),
+  jobsSchema.extend({
+    type: z.literal("jobs"),
+  }),
+  personsSchema.extend({
+    type: z.literal("persons"),
+  }),
+  pricingsSchema.extend({
+    type: z.literal("pricings"),
+  }),
+  productsSchema.extend({
+    type: z.literal("products"),
+  }),
+  quoteSchema.extend({
+    type: z.literal("quote"),
+  }),
+  reviewSchema.extend({
+    type: z.literal("review"),
+  }),
+  reviewsSchema.extend({
+    type: z.literal("reviews"),
+  }),
+  servicesSchema.extend({
+    type: z.literal("services"),
+  }),
+])
+
+export type SectionSchema = z.infer<typeof sectionSchema>
