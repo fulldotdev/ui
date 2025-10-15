06cc439bfd8bef2ee40ee91dae4a5055d942dfb3
Sil <silveltman@gmail.com>
2025-10-15 17:09:27 +0200

    components now extend html attrs

---
+++ src/components/ui/pattern/index.ts
@@ -0,0 +1,1 @@
+export { default as Pattern } from "./Pattern.astro"
--- src/components/ui/social/index.ts
+++ src/components/ui/social/index.ts
@@ -1,2 +1,1 @@
 export { default as Social } from "./Social.astro"
-export { default as SocialGroup } from "./SocialGroup.astro"
