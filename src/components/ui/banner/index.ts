b80b095a7530f57a3c2a08659ede4155bac1e263
Sil <silveltman@gmail.com>
2025-10-22 20:40:21 +0200

    minor fixes and improvements

---
+++ src/components/ui/banner/index.ts
@@ -0,0 +1,4 @@
+export { default as Banner } from "./banner.astro"
+export { default as BannerPanel } from "./banner-panel.astro"
+export { default as BannerContent } from "./banner-content.astro"
+export { default as BannerContainer } from "./banner-container.astro"
