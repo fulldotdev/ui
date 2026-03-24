bcba065947f99d7ddee6d725810a57d6796100a2
Sil <silveltman@gmail.com>
2026-03-24 18:00:42 +0100

    feat: add layout and container primitives

---
+++ src/components/ui/layout/index.ts
@@ -0,0 +1,4 @@
+export { default as Layout } from "./layout.astro"
+export { default as LayoutBody } from "./layout-body.astro"
+export { default as LayoutHead } from "./layout-head.astro"
+export { default as LayoutMain } from "./layout-main.astro"
