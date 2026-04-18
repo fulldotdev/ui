ef7165e11e551d92f5efc92fc19da432c9ad6939
Sil <silveltman@gmail.com>
2026-04-18 18:41:49 +0200

    refactor: align data-slot component wrappers

--- src/components/ui/tabs/index.ts
+++ src/components/ui/tabs/index.ts
@@ -1,4 +1,5 @@
 export { default as Tabs } from "./tabs.astro"
+export { default as TabsIndicator } from "./tabs-indicator.astro"
 export { default as TabsList } from "./tabs-list.astro"
 export { default as TabsTrigger } from "./tabs-trigger.astro"
 export { default as TabsContent } from "./tabs-content.astro"
