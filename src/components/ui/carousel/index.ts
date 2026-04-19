996a0bac06d56ad1e1322738a4df3151dc76dbe3
Sil <silveltman@gmail.com>
2026-04-19 16:41:02 +0200

    Add Embla-based carousel component

---
+++ src/components/ui/carousel/index.ts
@@ -0,0 +1,11 @@
+export type {
+  EmblaCarouselType as CarouselApi,
+  EmblaOptionsType as CarouselOptions,
+  EmblaPluginType as CarouselPlugin,
+} from "embla-carousel"
+
+export { default as Carousel } from "./carousel.astro"
+export { default as CarouselContent } from "./carousel-content.astro"
+export { default as CarouselItem } from "./carousel-item.astro"
+export { default as CarouselPrevious } from "./carousel-previous.astro"
+export { default as CarouselNext } from "./carousel-next.astro"
